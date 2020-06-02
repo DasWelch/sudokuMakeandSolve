4062850d6c2af6c6874057c36c83a1f5c89cbe6d
DasWelch <35710104+DasWelch@users.noreply.github.com>
2020-06-02 10:44:49 -0400

    added number in funciton to check if the number supplied is in that section of the board need to add 3x3 check

--- main.js
+++ main.js
@@ -7,14 +7,43 @@ function createBoard() {
     }
   }
 
-  board = starterboard(board);
+  //board = starterboard(board);
 
   return board;
 }
 
-function simpleNaiveBoardGenerator(Board){
-  
+function NumberInRow(num, Board, i) {
+  for (let r = 0; r < Board[i].length; r++) {
+    if (num === Board[i][r]) {
+      return true;
+    }
+  }
+  return false;
+}
 
+function NumberInCol(num, Board, j) {
+  for (let c = 0; c < Board.length; c++) {
+    if (num === Board[c][j]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function NumberIn3x3(num, Board, i, j) {}
+
+function simpleNaiveBoardGenerator(Board) {
+  for (let i = 0; i < Board.length; i++) {
+    for (let j = 0; j < Board[i].length; j++) {
+      let numWorks = false;
+      while (!numWorks) {
+        let numberForCell = Math.floor(Math.random() * (10 - 1)) + 1; // gives us 1 to 9 for random numbers
+        if (true) {
+          numWorks = true;
+        }
+      }
+    }
+  }
 }
 
 function starterboard(a) {
@@ -22,42 +51,46 @@ function starterboard(a) {
   let n = 1;
 
   for (let i = 0; i < 9; i++) {
-
     k = n;
 
-    for (let j = 0; j < 9; j++) {//this loop draws each line 
+    for (let j = 0; j < 9; j++) {
+      //this loop draws each line
 
-      if ((k <= 9)) {
+      if (k <= 9) {
         a[i][j] = k;
         k++;
-      }
-
-       else {
+      } else {
         k = 1;
         a[i][j] = k;
         k++;
       }
     }
-      console.log("n is "+n + " and k is " + k);
-      //more code here
-      n = k+3;
+    console.log("n is " + n + " and k is " + k);
+    //more code here
+    n = k + 3;
 
-      if(k===10){//this shifts each line down over by 3
-          n=4;
-      }
-      if(n>9){//this shifts us over by 1 
-          n=(n%9)+1
-      }
-
-    
+    if (k === 10) {
+      //this shifts each line down over by 3
+      n = 4;
+    }
+    if (n > 9) {
+      //this shifts us over by 1
+      n = (n % 9) + 1;
+    }
   }
   return a;
 }
 
-//this is the main
-
+//this is the main. made it to help my C# brain work in javascript better
 function main() {
-  console.log(createBoard());
+  let board = createBoard();
+  
+  //board[0] = [5, 5, 5, 5, 5, 5, 5, 5, 5];
+  for(let i =0; i< board.length; i++){
+    board[i][0] = 5;
+    }
+  console.log(NumberInCol(5,board,1));
+
 }
 
-main();// this is our program start 
+main(); // this is our program start
